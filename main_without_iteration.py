0993f84150fd30f3232763244ec803378785abb2
woffee <iwoffee@gmail.com>
2019-11-06 22:51:39 -0500

    backup

--- main_without_iteration.py
+++ main_without_iteration.py
@@ -1,5 +1,5 @@
 """
-Version 3，暂时不考虑迭代，假设分类已知，写一篇段文章出来
+Version 3，暂时不考虑迭代，假设分类已知
 """
 
 import pandas as pd
@@ -62,9 +62,6 @@ def lasso(x,D,y):
 
 
 def lessObsUpConstrain(x,D,y):
-    # print("x.shape:",x.shape)
-    # print("D.shape:",D.shape)
-    # print("y.shape:",y.shape)
     temp = (np.dot(D,x))/D.shape[1] - y
     eq = np.dot(temp,temp)
     return -eq+0.1
@@ -105,80 +102,6 @@ def minimizer_L1(x):
         print(result)
     return result.x
 
-
-def read_data(filepath, K, days, sample_size = 100):
-    # reading input data
-    # data contains:feature vector, state
-    data = pd.read_csv(filepath, encoding='utf-8')
-    data = data[:sample_size]
-
-    data.drop('user_id', axis=1, inplace=True)
-    data = data.dropna()
-    data.index = np.arange(len(data))
-
-    ## get the features of nodes ##
-    feature_sample = data[['FOLLOWERS_COUNT', 'FRIENDS_COUNT', 'STATUSES_COUNT', 'lat', 'lon']]
-    feature_sample.index = data.index
-    # feature_col = feature_sample.columns
-    ## rescale features to a compact cube ##
-    # feature_max = []
-    feature_sample['lon'] += 180
-    # print(feature_sample[['lat', 'lon']][:5])
-    for item in feature_sample.columns:
-        # feature_max.append(max(np.absolute(np.array(feature_sample[item]))))
-        feature_sample[item] /= max(np.absolute(np.array(feature_sample[item])))
-
-
-    ## define infect event and get 0-1 infection status sequence ##
-
-    spreading_key = []
-    for k in range(K):
-        for d in range(days):
-            spreading_key.append('k_' + str(k) + '_d_' + str(d))
-
-    spreading_sample = np.array(data[spreading_key])
-    spreading_sample = spreading_sample.reshape((sample_size * K, days))
-
-    # delete t with all zeros
-    all_zero_columns = np.where(~spreading_sample.any(axis=0))[0]
-    spreading_sample = np.delete(spreading_sample, all_zero_columns, axis=1)
-
-    # delete t which is not updated since last t
-    spreading_sample = spreading_sample.T
-    index = range(len(spreading_sample))
-    deleted = []
-    for i in range(len(spreading_sample)):
-        if i>0:
-            last = spreading_sample[i-1]
-            now  = spreading_sample[i]
-            if (last==now).all():
-                deleted.append(i)
-    print("deleted:", deleted)
-    exist = list(set(index).difference(set(deleted)))
-    # spreading_sample = np.delete(spreading_sample, deleted, axis=0)
-
-
-    random.shuffle(exist)
-    T1 = sorted(exist[0:int(len(exist)/2)])
-    T2 = sorted(exist[int(len(exist)/2):])
-
-    print(T1)
-    print(T2)
-    print("feature_sample:",feature_sample.shape)
-    print("spreading_sample:",spreading_sample.shape)
-
-
-    # np.savetxt(save_path + 'to_file_spreading_sample_all' + rundate + ".txt", spreading_sample)
-    # np.savetxt(save_path + 'to_file_spreading_sample_sub' + rundate + ".txt", spreading_sample[T1])
-
-    #
-    # # spreading_sample1 = spreading_sample1.reshape((sample_size * K / 2, days))
-    # spreading_sample2 = np.array(data[spreading_key2])
-    # # spreading_sample2 = spreading_sample2.reshape((sample_size * K / 2, days))
-    #
-
-    return feature_sample, spreading_sample, T1, T2
-
 def read_data_from_simulation(obs_filepath, true_net_filepath, K, days, sample_size = 100):
     data = pd.read_csv(true_net_filepath, encoding='utf-8')
 
@@ -193,7 +116,6 @@ def read_data_from_simulation(obs_filepath, true_net_filepath, K, days, sample_s
     for item in feature_sample.columns:
         feature_max.append(max(np.absolute(np.array(feature_sample[item]))))
         feature_sample[item] /= max(np.absolute(np.array(feature_sample[item])))
-    ## define infect event and get 0-1 infection status sequence ##
 
     # draw subsample nodes and spreading info on these nodes
     # data_sample=random.choice(data.index,size=3)
@@ -203,8 +125,8 @@ def read_data_from_simulation(obs_filepath, true_net_filepath, K, days, sample_s
 
     spreading_sample = pd.read_csv(obs_filepath, encoding='utf-8')
     # spreading_sample.drop('user_id', axis=1, inplace=True)
-    spreading_sample.drop([spreading_sample.columns[0]], axis=1, inplace=True)
-    spreading = spreading_sample.values
+    # spreading_sample.drop([spreading_sample.columns[0]], axis=1, inplace=True)
+    # spreading = spreading_sample.values
 
     spreading_sample = np.array(spreading_sample)
 
@@ -223,11 +145,7 @@ def read_data_from_simulation(obs_filepath, true_net_filepath, K, days, sample_s
                 deleted.append(i)
     print("deleted:", deleted)
     T = list(set(index).difference(set(deleted)))
-
-    # random.shuffle(exist)
-    # T1 = sorted(exist[0:int(len(exist) / 2)])
-    # T2 = sorted(exist[int(len(exist) / 2):])
-
+    print(T)
     print("features_matirx:",features.shape)
     print("spreading_sample:",spreading_sample.shape)
     return features, spreading_sample, T
@@ -241,35 +159,13 @@ def get_r_xit(x, i, t_l, features, spreading, K, T, bandwidth, dt):
     for j in range(features.shape[0]):
         x_j = features.iloc[j]
         g = gaussiankernel(x, x_j, bandwidth, features.shape[1])
-        # print("j,t_l,j*K+i:",j,t_l,j*K+i)
         tmp = spreading[t_l+1][j*K+i] - spreading[t_l][j*K+i]
-        # if tmp>0:
-        #     print("1.0*g*tmp",1.0*g*tmp)
         numerator = numerator + (1.0*g*tmp)
-
-        denominator = denominator + (g*(T[t_l+1]-T[t_l]))
-        # print("tmp:",tmp)
-        # print("T[t_l+1]:", T[t_l+1])
-    # print("numerator", numerator)
-    # if numerator == 0.0:
-    #     print("00.00")
-    #     print("x:", x)
-    #     print("i:", i)
-    #     print("t_l:", t_l)
-    #     print("T[t_l]:", T[t_l])
-    #     print(spreading[t_l])
-    #     print(spreading[t_l+1])
-    #     exit(0)
-    # print("numerator:", numerator)
-    # print("denominator:", denominator)
+        denominator = denominator + (g*(T[t_l+1]-T[t_l])*dt)
     return numerator/denominator
 
 
 def get_r_matrix(features, spreading, T, K=2, dt=0.01):
-    # print(features.shape)
-    # print(spreading.shape)
-    # (60, 5)
-    # (120, 16)
     bandwidth = np.diag(np.ones(features.shape[1]) * float(features.shape[0]) ** (-1. / float(features.shape[1] + 1)))
 
     r_matrix = []
@@ -306,6 +202,8 @@ def clear_zeros(mitrix):
 
 # 1.1 & 1.4
 def get_E(features, spreading, subT, K, dt=0.01):
+    logging.info("dt:" + str(dt))
+    print("dt:",dt)
     r_matrix = get_r_matrix(features, spreading, subT, K, dt)
     print("r_matrix: ", r_matrix.shape)
     logging.info("r_matrix.shape: " + str( r_matrix.shape))
@@ -318,7 +216,6 @@ def get_E(features, spreading, subT, K, dt=0.01):
     logging.info("subT:" + str(subT))
 
     np.savetxt(save_path + 'to_file_spreading_' + rundate + ".txt", spreading)
-
     np.savetxt(save_path + 'to_file_r_matrix_' + rundate + ".txt", r_matrix)
 
     cores = multiprocessing.cpu_count()
@@ -328,18 +225,11 @@ def get_E(features, spreading, subT, K, dt=0.01):
     # spreading = np.delete(spreading, -1, axis=0)
     # spreading = clear_zeros(spreading)
 
-
-
-
     xit_all = []
     for r_xit in r_matrix:
         xit_matrix = []
         xit_matrix.append(r_xit)  # y
         xit_matrix.append(spreading)  # D
-        # print("r_xit:",len(r_xit))
-        # print("spreading:",spreading.shape)
-        # r_xit: 15
-        # spreading: (15, 120)
         xit_all.append(xit_matrix)
     edge_list = pool.map(minimizer_L1, xit_all)
 
@@ -361,12 +251,12 @@ if __name__ == '__main__':
     days = 32
     sample_size = 100
 
-    obs_filepath = save_path+'/simulation/obs_1000x300.csv'
-    true_net_filepath = save_path+'/simulation/true_net_1000x300.csv'
-    feature_sample, spreading_sample, T1 = read_data_from_simulation(obs_filepath, true_net_filepath, K, days, sample_size=100)
+    obs_filepath = save_path+'/obs_net_hidden.csv'
+    true_net_filepath = save_path+'/true_net_net_hidden.csv'
+    feature_sample, spreading_sample, T = read_data_from_simulation(obs_filepath, true_net_filepath, K, days, sample_size=100)
 
     logging.info("start")
-    E = get_E(feature_sample, spreading_sample, T1, K, dt)
+    E = get_E(feature_sample, spreading_sample, T, K, dt)
     save_E(E, save_path + "to_file_E_" + rundate + ".csv")
     logging.info("done")
 
