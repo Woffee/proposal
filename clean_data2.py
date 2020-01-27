c39864720a05d1ee4075ebde1bf57ade0535c7ad
woffee <iwoffee@gmail.com>
2020-01-27 18:15:49 -0500

    twitter test

--- clean_data2.py
+++ clean_data2.py
@@ -15,12 +15,68 @@ import pickle
 import os
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from sklearn.cluster import KMeans
-# import logging
-import queue
+from nltk.tokenize import word_tokenize
+import nltk
+# nltk.download('punkt')
+
+
 pd.set_option('display.max_columns', 500)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SAVE_PATH = BASE_DIR + '/data_twitter'
+K = 2
+
+class MyDoc2vec():
+
+    def train(self, data):
+        # data = ["I love machine learning. Its awesome.",
+        #         "I love coding in python",
+        #         "I love building chatbots",
+        #         "they chat amagingly well"]
+
+        tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(data)]
+
+        max_epochs = 10
+        vec_size = 300
+        alpha = 0.025
+
+        model = Doc2Vec(size=vec_size,
+                        alpha=alpha,
+                        min_alpha=0.00025,
+                        min_count=2,
+                        dm=1)
+        model.build_vocab(tagged_data)
+        print("training d2v model...")
+        for epoch in range(max_epochs):
+            print('iteration {0}'.format(epoch))
+            model.train(tagged_data,
+                        total_examples=model.corpus_count,
+                        epochs=model.epochs)
+            # decrease the learning rate
+            model.alpha -= 0.0002
+            # fix the learning rate, no decay
+            model.min_alpha = model.alpha
+
+        model.save(SAVE_PATH + "/d2v.model")
+        return model
+
+    def get_vecs(self, data):
+        if os.path.exists(SAVE_PATH + "/d2v.model"):
+            model = Doc2Vec.load(SAVE_PATH + "/d2v.model")
+        else:
+            model = self.train(data)
+
+        vecs = []
+        for d in data:
+            test_data = word_tokenize(d.lower())
+            vecs.append( model.infer_vector(test_data))
+        return vecs
+
+    def get_labels(self, data):
+        vecs = self.get_vecs(data)
+        y_pred = KMeans(n_clusters=K, random_state=9).fit_predict(vecs)
+        return y_pred
+
 
 def read_data_from_xls(filepath):
     filelist = os.listdir(filepath)
@@ -45,6 +101,9 @@ def read_data_from_xls(filepath):
     usernames = set(tweet.FROM_USER)
     print("len(usernames):", len(usernames))
 
+    d2v = MyDoc2vec()
+    labels = d2v.get_labels(list(tweet.TEXT_))
+
     obs = []
     features = []
     date_list = [20170811 + i for i in range(21)] + [20170901 + i for i in range(11)]
@@ -54,14 +113,26 @@ def read_data_from_xls(filepath):
         feature = userdata.iloc[0, 4:]
         features.append(list(feature))
 
-        raw = []
-        last_num = 0
+        status_0 = []
+        status_1 = []
+
         for d in date_list:
-            today = 1 if len(userdata[userdata.CREATED_AT == d])>0 else 0
-            num = today + last_num
-            raw.append(num)
-            last_num = num
-        obs.append(raw)
+            today_data = userdata[userdata.CREATED_AT == d]
+            today_status = [0, 0]
+            for index, row in today_data.iterrows():
+                today_status[ labels[index] ] = 1
+            status_0.append(today_status[0])
+            status_1.append(today_status[1])
+
+        for i in range(len(status_0)):
+            if i>0:
+                status_0[i] += status_0[i-1]
+        for i in range(len(status_1)):
+            if i>0:
+                status_1[i] += status_1[i-1]
+
+        obs.append(status_0)
+        obs.append(status_1)
 
         if len(features)>= 300:
             break
